ac3ca006370490cb055ab33d658ed98152f13c9e
isabelxklee <hi@kleetime.com>
2024-09-25 14:20:27 -0400

    imported font files

--- app/globalStyles.js
+++ app/globalStyles.js
@@ -3,8 +3,17 @@
 import styled, { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Authentic Sans';
+    font-style: regular;
+    font-weight: 400;
+    src: url('Authentic Sans') format('woff')
+  }
+
   body {
     margin: 0;
     padding: 0;
+    font-family: "Fann Grotesque Pro", sans-serif;
+    font-weight: normal;
   }
 `;
