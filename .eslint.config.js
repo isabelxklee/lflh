1c1b689d01d8c681d2b3ec53ba2d8d7ab05d9144
isabelxklee <hi@kleetime.com>
2024-09-25 13:50:38 -0400

    updated eslint

--- .eslint.config.js
+++ .eslint.config.js
@@ -1,13 +1,13 @@
 import js from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default [
   js.configs.recommended,
-
   {
     rules: {
       'no-unused-vars': 'warn',
       'no-undef': 'warn'
-    },
-    extends: ['prettier']
-  }
+    }
+  },
+  eslintConfigPrettier
 ]
