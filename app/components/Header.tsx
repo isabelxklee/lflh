f4a39453541064e883b030176e05a5fe10ddb6ed
isabelxklee <hi@kleetime.com>
2024-09-25 14:11:31 -0400

    added more links to header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Link from 'next/link';
 import styled from 'styled-components';
 
@@ -11,6 +13,24 @@ const Header = () => {
       <ul>
         <li>
           <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/oral-histories">Oral Histories</Link>
+        </li>
+        <li>
+          <Link href="/themes">Themes</Link>
+        </li>
+        <li>
+          <Link href="/resources">Resources</Link>
+        </li>
+        <li>
+          <Link href="/partners">Partners</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
         </li>
       </ul>
     </Wrapper>
