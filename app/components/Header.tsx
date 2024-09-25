f02caf0b6eb660fefb1c2b42cf5f40a56760a475
isabelxklee <hi@kleetime.com>
2024-09-25 14:14:43 -0400

    styled header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,34 +5,47 @@ import styled from 'styled-components';
 
 const Wrapper = styled.header`
   padding: 20px;
+  background: grey;
+`;
+
+const UL = styled.ul`
+  list-style: none;
+  display: flex;
+  gap: 40px;
+  justify-content: center;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
 `;
 
 const Header = () => {
   return (
     <Wrapper>
-      <ul>
+      <UL>
         <li>
-          <Link href="/">Home</Link>
+          <StyledLink href="/">Home</StyledLink>
         </li>
         <li>
-          <Link href="/about">About</Link>
+          <StyledLink href="/about">About</StyledLink>
         </li>
         <li>
-          <Link href="/oral-histories">Oral Histories</Link>
+          <StyledLink href="/oral-histories">Oral Histories</StyledLink>
         </li>
         <li>
-          <Link href="/themes">Themes</Link>
+          <StyledLink href="/themes">Themes</StyledLink>
         </li>
         <li>
-          <Link href="/resources">Resources</Link>
+          <StyledLink href="/resources">Resources</StyledLink>
         </li>
         <li>
-          <Link href="/partners">Partners</Link>
+          <StyledLink href="/partners">Partners</StyledLink>
         </li>
         <li>
-          <Link href="/contact">Contact</Link>
+          <StyledLink href="/contact">Contact</StyledLink>
         </li>
-      </ul>
+      </UL>
     </Wrapper>
   );
 };
--- app/globalStyles.js
+++ app/globalStyles.js
@@ -4,9 +4,5 @@ export const GlobalStyles = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    font-family: 'Cecilie Sans', sans-serif;
-    font-weight: ${FONT_WEIGHTS.BOOK};
-    background: ${COLORS.WHITE};
-    color: ${COLORS.BLACK};
   }
 `;
