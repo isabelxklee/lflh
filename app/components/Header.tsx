9b6cd571042b227929e456bc535c902f32e41c5c
isabelxklee <hi@kleetime.com>
2024-09-25 14:09:13 -0400

    created wrapper and links for header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,20 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const Wrapper = styled.header`
+  padding: 20px;
+`;
+
 const Header = () => {
-  return <></>;
+  return (
+    <Wrapper>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+      </ul>
+    </Wrapper>
+  );
 };
 
 export default Header;
