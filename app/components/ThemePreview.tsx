648c4c76c9503c2353e9a2074183953de030d6f9
isabelxklee <hi@kleetime.com>
2024-09-28 14:41:59 -0400

    created theme preview component

---
+++ app/components/ThemePreview.tsx
@@ -0,0 +1,18 @@
+const ThemePreview = () => {
+  return (
+    <>
+      <h2>The Body: As an Epistemological Site</h2>
+      <p>
+        I started questioning everything in my life. I realized—I got diagnosed
+        with autism while during COVID and I started the unmasking process of
+        just peeling back. 'Okay, I do this to please other people. I say these
+        things to please other people, I say this to fit in' and really
+        discovering who I was.
+      </p>
+      <button>Play Interview</button>
+      <button>Explore Theme</button>
+    </>
+  );
+};
+
+export default ThemePreview;
