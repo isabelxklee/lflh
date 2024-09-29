3a7705a942cfedc33835514d94afb96fa6134ec7
isabelxklee <hi@kleetime.com>
2024-09-29 12:14:16 -0400

    updated sanity config schema

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
 import { media } from 'sanity-plugin-media';
 import { structure } from './sanity/structure';
-// import { schemaTypes } from './sanity/schemas';
+import { postType } from './sanity/schemaTypes/postType';
 import { visionTool } from '@sanity/vision';
 
 const config = defineConfig({
@@ -11,10 +11,10 @@ const config = defineConfig({
   title: 'Listening for the Long Haul',
   apiVersion: '2024-09-29',
   basePath: '/admin',
-  plugins: [structureTool({ structure: structure }), media(), visionTool()]
-  // schema: {
-  //   types: schemaTypes
-  // }
+  plugins: [structureTool({ structure: structure }), media(), visionTool()],
+  schema: {
+    types: [postType]
+  }
 });
 
 export default config;
