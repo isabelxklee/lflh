ec411dc455052c1b5c1d7929066cf69c3ab5d2d2
isabelxklee <hi@kleetime.com>
2024-10-02 18:32:49 -0400

    updated text preview

--- app/components/ThemePreview.tsx
+++ app/components/ThemePreview.tsx
@@ -1,17 +1,32 @@
+import styled from 'styled-components';
 import { H3, P, TextLink } from '../globalStyles';
 
 interface ThemePreviewProps {
   theme: any;
 }
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
+`;
+
+const LinkWrapper = styled.div`
+  display: flex;
+  gap: 40px;
+`;
+
 const ThemePreview = ({ theme }: ThemePreviewProps) => {
   return (
-    <>
+    <Wrapper>
       <H3>{theme.title}</H3>
       <P>{theme.body}</P>
-      <TextLink href="/">Play Interview</TextLink>
-      <TextLink href="/">Explore Theme</TextLink>
-    </>
+      <LinkWrapper>
+        <TextLink href="/">Play Interview</TextLink>
+        <TextLink href="/">Explore Theme</TextLink>
+      </LinkWrapper>
+    </Wrapper>
   );
 };
 
