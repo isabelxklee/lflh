31f18bbd4d6a4817f86379a8235d7ba5a6ff95ab
isabelxklee <hi@kleetime.com>
2024-10-07 21:07:57 -0400

    created styled link for itnerview titles

--- app/(pages)/oral-histories/page.tsx
+++ app/(pages)/oral-histories/page.tsx
@@ -5,12 +5,23 @@ import styled from 'styled-components';
 import { useCallback } from 'react';
 import Header from '../../components/Header';
 import { MiniGradientHeader } from '../../components/GradientHeader';
+import Link from 'next/link.js';
+import { COLORS, FONT_WEIGHTS, FONTS } from '../../globalStyles.js';
 
 const ExcerptWrapper = styled.div<{ $length: number }>``;
 
 const Wrapper = styled.div`
   padding: 200px 25%;
   margin-bottom: 200px;
+`;
+
+const StyledLink = styled(Link)`
+  font-family: ${FONTS.AUTH_SANS};
+  font-weight: ${FONT_WEIGHTS.BOLD};
+  font-size: 14px;
+  color: ${COLORS.BLACK};
+  text-decoration: none;
+  margin: 0;
 `;
 
 export default function OralHistories() {
@@ -28,7 +39,7 @@ export default function OralHistories() {
         <h1>Oral Histories</h1>
         {interviews.map((interview: any, index: number) => (
           <div key={index}>
-            <p>{interview.title}</p>
+            <StyledLink href="/">{interview.title}</StyledLink>
             {interview.excerpts.map((excerpt: any, index: number) => (
               <ExcerptWrapper key={index} $length={excerpt.startTime}>
                 <p>{excerpt.subTheme}</p>
