da1110d36c4dc8e3a6437664ca96925c385bfa44
isabelxklee <hi@kleetime.com>
2024-10-08 12:05:26 -0400

    updated template logic

--- app/template.tsx
+++ app/template.tsx
@@ -1,13 +1,29 @@
-import { ReactNode } from 'react';
+'use client';
+
+import { ReactNode, useMemo } from 'react';
 import { MiniGradientHeader } from './components/GradientHeader';
 import Header from './components/Header';
+import { usePathname } from 'next/navigation';
 
 export default function PageTemplate({ children }: { children: ReactNode }) {
+  // don't return anything if pathname is '/'
+  const pathname = usePathname();
+
+  const isHomePage = useMemo(() => {
+    return pathname === '/';
+  }, []);
+
   return (
     <>
-      <Header show={true} />
-      <MiniGradientHeader />
-      {children}
+      {isHomePage ? (
+        <>{children}</>
+      ) : (
+        <>
+          <Header show={true} />
+          <MiniGradientHeader />
+          {children}
+        </>
+      )}
     </>
   );
 }
