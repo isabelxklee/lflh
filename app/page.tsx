9b86f627726dc154d3d0f050122767a14f1ee8dc
isabelxklee <hi@kleetime.com>
2024-10-09 12:56:02 -0400

    fixed sanity issue by updating project id

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 'use server';
 
-import { client } from '../sanity/lib/client';
-import { SUB_THEMES_QUERY, THEMES_QUERY } from '../sanity/lib/queries';
 import { getSubThemes, getThemes } from '../sanity/sanity.utils';
 import Home from './(pages)';
 
 export default async function Index() {
-  // const themes = await getThemes();
-  // const subThemes = await getSubThemes();
-  const themes = await client.fetch(THEMES_QUERY);
-  const subThemes = await client.fetch(SUB_THEMES_QUERY);
+  const themes = await getThemes();
+  const subThemes = await getSubThemes();
 
   return (
     <>
