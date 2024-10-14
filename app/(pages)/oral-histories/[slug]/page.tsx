f7b519afd1b185d7142f183b7b65ff68d085a19c
isabelxklee <hi@kleetime.com>
2024-10-13 22:36:58 -0400

    implementing use pathname

--- app/(pages)/oral-histories/[slug]/page.tsx
+++ app/(pages)/oral-histories/[slug]/page.tsx
@@ -1,7 +1,15 @@
+'use client';
+
+import styled from 'styled-components';
+import { usePathname } from 'next/navigation';
+
+const Wrapper = styled.div`
+  padding: 200px 25%;
+  margin-bottom: 200px;
+`;
+
 export default function InterviewPage() {
-  return (
-    <>
-      <p>hello world</p>
-    </>
-  );
+  const pathname = usePathname();
+  console.log(pathname);
+  return <Wrapper>{/* <p>{title}</p> */}</Wrapper>;
 }
