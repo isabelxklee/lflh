d8dd97bc5e34e6094dfc905d4605850356e08df0
isabelxklee <hi@kleetime.com>
2024-10-22 16:31:51 -0400

    updated transcript style

--- app/components/Transcript.tsx
+++ app/components/Transcript.tsx
@@ -1,14 +1,19 @@
 import { useCallback } from 'react';
 import { P } from '../globalStyles';
+import { styled } from 'styled-components';
 
 interface TranscriptProps {
   text: string;
 }
 
-export default function Transcript({ text }: any) {
+const TextWrapper = styled.div`
+  margin-bottom: 32px;
+`;
+
+export default function Transcript({ text }: TranscriptProps) {
   return (
-    <>
+    <TextWrapper>
       <P>{text}</P>
-    </>
+    </TextWrapper>
   );
 }
