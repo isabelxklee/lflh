b7a134d3908a7898c2e92c03ee8f9137823b06c3
isabelxklee <hi@kleetime.com>
2024-10-22 17:14:30 -0400

    updated types

--- sanity/types/types.ts
+++ sanity/types/types.ts
@@ -11,3 +11,22 @@ export type SubThemeType = {
   sort: number;
   themeName: string;
 };
+
+export type InterviewType = {
+  _id: string;
+  title: string;
+  audioFile: any;
+  audioFileURL: string;
+  transcript: string;
+  transcriptText: string;
+  slug: string;
+};
+
+export type ExcerptType = {
+  _id: string;
+  title: string;
+  startTime: string;
+  endTime: string;
+  subTheme: any;
+  interview: any;
+};
