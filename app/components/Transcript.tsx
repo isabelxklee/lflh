af3061cc836126477c7a0fd69446e2d50d1f8ace
isabelxklee <hi@kleetime.com>
2024-10-22 17:05:49 -0400

    updated string strip logic

--- app/components/Transcript.tsx
+++ app/components/Transcript.tsx
@@ -14,12 +14,22 @@ const TextWrapper = styled.div`
 export default function Transcript({ text }: TranscriptProps) {
   const [speaker, setSpeaker] = useState<string>('');
   const [cleanText, setCleanText] = useState<string>('');
+  const [timeStamp, setTimeStamp] = useState<string>('');
 
   useEffect(() => {
     const index = text.indexOf(':');
     setSpeaker(text.slice(0, index));
+    const regex = /(\([0-9:]{5,8})/g;
+    const ts = text.match(regex);
 
-    setCleanText(text.slice(index + 2, text.length));
+    if (ts) {
+      const cleanTs = ts[0].slice(1, ts[0].length);
+      setTimeStamp(cleanTs);
+      const closingIndex = text.indexOf(ts[0]);
+      setCleanText(text.slice(index + 2, closingIndex));
+    } else {
+      setCleanText(text.slice(index + 2, text.length));
+    }
   }, [text]);
 
   return (
