0012ab3cc5042644083706b08bb2161f21c48358
isabelxklee <hi@kleetime.com>
2024-10-21 21:13:00 -0400

    updated transcript component

--- app/components/Transcript.tsx
+++ app/components/Transcript.tsx
@@ -1,5 +1,14 @@
 import { PortableText } from '@portabletext/react';
 
-export default function Transcript() {
-  return <></>;
+export default function Transcript({ transcript }: any) {
+  const components = {
+    block: {
+      h1: ({ children }: any) => <h1 className="text-2xl">{children}</h1>,
+      blockquote: ({ children }: any) => (
+        <blockquote className="border-l-purple-500">{children}</blockquote>
+      )
+    }
+  };
+
+  return <PortableText value={transcript} components={components} />;
 }
