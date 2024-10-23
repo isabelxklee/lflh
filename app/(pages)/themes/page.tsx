906270e9df1f4f97e5c7c24ae8bc56513c61b317
isabelxklee <hi@kleetime.com>
2024-10-23 12:07:29 -0400

    updated themes page

--- app/(pages)/themes/page.tsx
+++ app/(pages)/themes/page.tsx
@@ -1,4 +1,20 @@
+import { useState, useEffect } from 'react';
+import { getSiteSettings } from '../../actions';
+
 export default function Themes() {
+  const [siteSettings, setSiteSettings] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const siteSettings = await getSiteSettings();
+      setSiteSettings(siteSettings);
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(siteSettings);
+
   return (
     <>
       <h1>Themes</h1>
