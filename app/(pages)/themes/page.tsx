447374aec80e20f26a46b595ce9be852f094a3f5
isabelxklee <hi@kleetime.com>
2024-10-23 12:58:00 -0400

    created a list

--- app/(pages)/themes/page.tsx
+++ app/(pages)/themes/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { getSiteSettings, getThemes } from '../../actions';
-import { P, H3 } from '../../styles';
+import { P, H3, COLORS, FONT_WEIGHTS, FONTS } from '../../styles';
 import styled from 'styled-components';
 import { ThemeType } from '../../../sanity/types/types';
 import Link from 'next/link';
@@ -14,6 +14,19 @@ const Wrapper = styled.div`
 
 const ThemeWrapper = styled.div`
   margin-bottom: 32px;
+`;
+
+const StyledLink = styled(Link)`
+  font-family: ${FONTS.AUTH_SANS}, sans-serif;
+  font-weight: ${FONT_WEIGHTS.MEDIUM};
+  font-size: 18px;
+  color: ${COLORS.BLACK};
+  text-decoration: none;
+  margin: 0;
+
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
 export default function Themes() {
@@ -39,11 +52,15 @@ export default function Themes() {
       {themes.map((theme, index: number) => (
         <ThemeWrapper key={index}>
           <H3>{theme.title}</H3>
-          {theme.subThemes.map((subTheme, index: number) => (
-            <Link key={index} href={`/themes/${subTheme.slug}`}>
-              {subTheme.title}
-            </Link>
-          ))}
+          <ul>
+            {theme.subThemes.map((subTheme, index: number) => (
+              <li>
+                <StyledLink key={index} href={`/themes/${subTheme.slug}`}>
+                  {subTheme.title}
+                </StyledLink>
+              </li>
+            ))}
+          </ul>
         </ThemeWrapper>
       ))}
     </Wrapper>
