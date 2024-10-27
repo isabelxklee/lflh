48a8568af933f8d7b89cf1893b63d777f1d0e2ce
isabelxklee <hi@kleetime.com>
2024-10-27 13:52:46 -0400

    created page title component and updated contact page

--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -1,7 +1,19 @@
+import { P, PageTitle, PageWrapper } from '../../styles';
+
 export default function Contact() {
   return (
-    <>
-      <h1>Contact</h1>
-    </>
+    <PageWrapper>
+      <PageTitle>Contact us</PageTitle>
+      <P>
+        About page description project of people living with Long COVID and
+        associated conditions (pwLCAC). In this section of the exhibition we
+        encourage you find a quiet space where you can listen to, and process,
+        the audio clips from the interviews. Here you will hear original
+        interviews, and also in the way the exhibition is organized, with one
+        clip following the other and organized by theme. To keep this section
+        accessible, we have included the transcript with each audio clip, so
+        that you can read while you listen and listen while you read.
+      </P>
+    </PageWrapper>
   );
 }
