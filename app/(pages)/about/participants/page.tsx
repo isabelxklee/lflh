375011446fa8f7d6fe081c799655b57d60fb7326
isabelxklee <hi@kleetime.com>
2024-10-27 13:33:30 -0400

    added athena

--- app/(pages)/about/participants/page.tsx
+++ app/(pages)/about/participants/page.tsx
@@ -12,7 +12,7 @@ import { TextLink } from '../page';
 import { PlayIcon } from '../../../components/AudioPlayer/Controls';
 
 const PersonWrapper = styled.div`
-  margin: 50px 0 32px 0;
+  margin: 50px 0;
   display: flex;
 `;
 
@@ -57,6 +57,28 @@ export default function ParticipantsPage() {
           <RighTextLink href="/">
             <PlayIcon />
             Listen to Lygia's story
+          </RighTextLink>
+        </TextWrapper>
+      </PersonWrapper>
+      <PersonWrapper>
+        <AuthP style={{ flex: 1, fontWeight: FONT_WEIGHTS.BOLD }}>Athena</AuthP>
+        <TextWrapper>
+          <P>
+            Bio paragraph for Athena. For me personally, I think, it’s more
+            meaningful to my family and friends. Only because they know me best.
+            I mean, I would like to share it with the world as far as, helping
+            advocate, for others that are also dealing with chronic illnesses in
+            particular long COVID. As well as validate their feelings and what
+            they’re going through, because I know for myself, especially early
+            with getting COVID, early on in the pandemic. I was ill with Covid
+            in April of 2020. So it was during that first wave of, the pandemic,
+            when, literally millions of people were dying throughout the world.
+            There were no real precautions yet, a lot of confusion, and the
+            vaccines were not developed yet.
+          </P>
+          <RighTextLink href="/">
+            <PlayIcon />
+            Listen to Athena's story
           </RighTextLink>
         </TextWrapper>
       </PersonWrapper>
