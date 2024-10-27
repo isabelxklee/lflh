ea4e8f4506cacd0e845db51584d690768888eb35
isabelxklee <hi@kleetime.com>
2024-10-27 13:54:39 -0400

    updated partners page

--- app/(pages)/resources/page.tsx
+++ app/(pages)/resources/page.tsx
@@ -1,7 +1,23 @@
+import { P, PageTitle, PageWrapper } from '../../styles';
+
 export default function Resources() {
   return (
-    <>
-      <h1>Resources</h1>
-    </>
+    <PageWrapper>
+      <PageTitle>Resources</PageTitle>
+      <P>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+        tristique et odio vitae aliquam. Nunc vitae sapien ac sapien porta
+        maximus vitae quis ante. Phasellus quis tortor nibh. Proin varius felis
+        eu rutrum elementum. Integer ut faucibus lectus, et tincidunt lorem.
+        Morbi suscipit mollis libero, ac interdum ligula cursus at. Sed vel
+        metus nec diam suscipit feugiat eu et sapien. Suspendisse aliquet odio
+        sed odio ultrices sollicitudin. Etiam sit amet gravida tellus. Quisque
+        non elit ac augue auctor scelerisque eget eget erat. Proin gravida,
+        risus sit amet tristique lacinia, est felis viverra nulla, in
+        ullamcorper justo lorem eu ligula. Nam ac vestibulum dui, sit amet
+        tempor ipsum. Aliquam auctor tellus eu libero lacinia tempus. Vivamus
+        placerat lacinia efficitur.
+      </P>
+    </PageWrapper>
   );
 }
